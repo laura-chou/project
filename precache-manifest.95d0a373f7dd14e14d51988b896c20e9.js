8cc4e596e03439fb71a5654c0b478568eca0044f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-06 04:24:07 +0000

    Deploy laura-chou/project to github.com/laura-chou/project.git:gh-pages

---
+++ precache-manifest.95d0a373f7dd14e14d51988b896c20e9.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3af8c7421a018650eaff",
+    "url": "/css/app.473c022e.css"
+  },
+  {
+    "revision": "5e10b03a5c4bd14bbd29",
+    "url": "/css/chunk-vendors.af595ca4.css"
+  },
+  {
+    "revision": "cd8db2c5ec7b8f4440a68e92be276e8f",
+    "url": "/img/about.png"
+  },
+  {
+    "revision": "f7cebed1b930f018e57cca26f08771a4",
+    "url": "/img/address.png"
+  },
+  {
+    "revision": "522b3d222081dca8f190f65782029c3e",
+    "url": "/img/bus.png"
+  },
+  {
+    "revision": "decb4aedff4f8121fedcc2cde0e65275",
+    "url": "/img/clock.png"
+  },
+  {
+    "revision": "557f8023c8f1bc41dac03ec44927a5cf",
+    "url": "/img/employee.png"
+  },
+  {
+    "revision": "cae1d9609a48ade71165abf299eaf58b",
+    "url": "/img/facebook.png"
+  },
+  {
+    "revision": "11be7417e9c7ab99fd2746ed0b8c0c03",
+    "url": "/img/instagram.png"
+  },
+  {
+    "revision": "b81333ca6d1cf2e51a6cff84ca95705c",
+    "url": "/img/location.png"
+  },
+  {
+    "revision": "c74f64f94351f7856a907c62a28aea03",
+    "url": "/img/login.png"
+  },
+  {
+    "revision": "28812e9a5701cf7f04a93b27dff860fd",
+    "url": "/img/logo.png"
+  },
+  {
+    "revision": "754d11676d7fd55ebd86325e4a764971",
+    "url": "/img/logout.png"
+  },
+  {
+    "revision": "41992b270f34a545277387dba63f93db",
+    "url": "/img/menu.png"
+  },
+  {
+    "revision": "936da0f40e670d6625c8a44732f0a689",
+    "url": "/img/message.png"
+  },
+  {
+    "revision": "c80e71c1273622d721307890b0952a9a",
+    "url": "/img/metro.png"
+  },
+  {
+    "revision": "49d9515832d6c8b6e438edcc25f58e5a",
+    "url": "/img/navigation_close.png"
+  },
+  {
+    "revision": "cec06aace96111592ef33366cb2579b4",
+    "url": "/img/navigation_open.png"
+  },
+  {
+    "revision": "6ad45ad415c3a5538dfdff4dae1a3c5c",
+    "url": "/img/new.png"
+  },
+  {
+    "revision": "bd54360c4754aa424c0de6ceec76e485",
+    "url": "/img/password.png"
+  },
+  {
+    "revision": "dd269dfde188b1fd9cf88d06988bd738",
+    "url": "/img/pencil.png"
+  },
+  {
+    "revision": "c5a401f609c891e997138b3c05a4ce78",
+    "url": "/img/phone.png"
+  },
+  {
+    "revision": "280f1152706bf317b48ec5810baf4908",
+    "url": "/img/photo.png"
+  },
+  {
+    "revision": "ac11b4bd91809e8991f80cb6889a1dae",
+    "url": "/img/send.png"
+  },
+  {
+    "revision": "fb1ae5e325c4b31bbd34b826398ec57d",
+    "url": "/img/take-away.png"
+  },
+  {
+    "revision": "79044edb1901d358c335d8e0265101df",
+    "url": "/img/user.png"
+  },
+  {
+    "revision": "8728598f1c48a058dcd49e8a31a87fc7",
+    "url": "/img/warn.png"
+  },
+  {
+    "revision": "bc9c66db6f96bd2f4248932a506ccc91",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3af8c7421a018650eaff",
+    "url": "/js/app.308b88a5.js"
+  },
+  {
+    "revision": "1de313d4981b5ef8f8e3",
+    "url": "/js/backstage.b9d12ae8.js"
+  },
+  {
+    "revision": "11f75e8896284d0c93aa",
+    "url": "/js/check.305ada0f.js"
+  },
+  {
+    "revision": "5e10b03a5c4bd14bbd29",
+    "url": "/js/chunk-vendors.1a870509.js"
+  },
+  {
+    "revision": "70715d83e1c65b42ed77",
+    "url": "/js/order.b10ec951.js"
+  },
+  {
+    "revision": "1ecee0c343b1bc0443f2e6eba14ed6ff",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
